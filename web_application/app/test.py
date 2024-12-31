bb7926f632a8e4975c3325be3438a294a981e8cc
Vahidr32 <rafieiva@msu.edu>
2024-12-30 21:31:21 -0500

    update

--- web_application/app/test.py
+++ web_application/app/test.py
@@ -5,20 +5,13 @@ def hydrogeo_dataset_dict(path=None):
 	path = "/data/MyDataBase/HydroGeoDataset/HydroGeoDataset_ML_250.h5"
 	with h5py.File(path,'r') as f:
 		groups = f.keys()
-		hydrogeo_dict = {}
-		for group in groups:	
-			hydrogeo_dict[group] = list(f[group].keys())
+		hydrogeo_dict = {group: list(f[group].keys()) for group in groups}
 	return hydrogeo_dict
 
 def get_subvariables(variable):
-
-
-    # Fetch subvariables for the selected variable
-    hydrodict = hydrogeo_dataset_dict()
-
-    # Check if the variable exists in the dataset
-    subvariables = hydrodict.get(variable, None)
-    return subvariables
+	# Fetch subvariables for the selected variable
+	hydrodict = hydrogeo_dataset_dict()
+	return hydrodict.get(variable, None)
 
 if __name__ == "__main__":
 	variable = "geospatial"
