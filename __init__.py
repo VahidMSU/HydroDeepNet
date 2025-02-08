a2b1e8735639698d7aa88f2645ad0d5dd957d058
Vahid Rafiei <menly42@gmail.com>
2025-02-08 14:23:41 -0500

    update visualziation page

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-def create_app():
-    app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
-    CORS(app)
-    return app
