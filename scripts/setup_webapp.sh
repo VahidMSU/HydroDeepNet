509980c1277f646be2ba823efdc38e0575003df0
Vahidr32 <rafieiva@msu.edu>
2025-02-08 23:02:51 -0500

    update

--- scripts/setup_webapp.sh
+++ scripts/setup_webapp.sh
@@ -14,13 +14,13 @@ sudo systemctl restart apache2
 cd /data/SWATGenXApp/codes/web_application/frontend
 
 # Update package-lock.json to match package.json
-npm install
+#npm install
 
 # Use npm ci for faster, more reliable builds
-npm ci
+#npm ci
 
 # Run update, audit fix, and build in parallel
-npx concurrently "npm update" "npm audit fix" "npm run build"
-
+#npx concurrently "npm update" "npm audit fix" "npm run build"
+npm run build
 # Start the application
 npm start
